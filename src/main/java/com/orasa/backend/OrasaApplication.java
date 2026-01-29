214fc5c700e49a111585c6335460a64d39d65a28
Adrian <adrianvillamin0612@gmail.com>
2026-01-29 13:42:46 +0800

    Initial commit

---
+++ src/main/java/com/orasa/backend/OrasaApplication.java
@@ -0,0 +1,13 @@
+package com.orasa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrasaApplication.class, args);
+	}
+
+}
