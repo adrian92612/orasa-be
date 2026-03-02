2ebe8ae1459d9f0ba4237d6fb1f412b4243e690b
Adrian <adrianvillamin0612@gmail.com>
2026-03-02 10:01:00 +0800

    reverted to httponly cookie

--- src/main/java/com/orasa/backend/controller/AuthController.java
+++ src/main/java/com/orasa/backend/controller/AuthController.java
@@ -103,7 +103,7 @@ public class AuthController extends BaseController {
 
   private void addTokenCookie(HttpServletResponse response, String token) {
     ResponseCookie cookie = ResponseCookie.from("token", token)
-        .httpOnly(false)
+        .httpOnly(true)
         .secure(true)
         .path("/")
         .maxAge(orasaProperties.getJwt().getExpiration() / 1000)
