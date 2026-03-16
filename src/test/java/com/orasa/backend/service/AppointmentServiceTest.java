52a087c03236da8f3cf98bb13ee6eedce8df011b
Adrian <adrianvillamin0612@gmail.com>
2026-03-16 10:32:22 +0800

    fixed node 24 warning

--- src/test/java/com/orasa/backend/service/AppointmentServiceTest.java
+++ src/test/java/com/orasa/backend/service/AppointmentServiceTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
